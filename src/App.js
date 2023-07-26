fc61528f0304b13a6674520b66b45d65e545657a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-07-26 21:54:16 +0300

    fianl commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,58 @@ import SubmitButton from './SubmitButton';
 import InputField from './InputField';
 
 function App() {
-async compo
+    async await componentDidMount(){
+  try{
+    let res = await fetch('/isLoggedIn',{
+      method:'post',
+      headers:{
+        'Accept':'application/json',
+        'Content-type':'application/json'
+      }
+    });
+    let result = await res.json();
+
+    if(result && result.success){
+      UserStore.loading = false;
+      UserStore.isLoggedIn = true;
+      UserStore.username = result.username;
+    }
+    else{
+      UserStore.loadiing = false;
+      UserStore.isLoggedIn= false;
+    }
+  }
+  catch(e){
+UserStore.loading = false;
+UserStore.isLoggedIn= false;
+  }
+}
+async  doLogout(){
+  try{
+    let res = await fetch('/isLoggedIn',{
+      method:'post',
+      headers:{
+        'Accept':'application/json',
+        'Content-type':'application/json'
+      }
+    });
+    let result = await res.json();
+
+    if(result && result.success){
+      UserStore.loading = false;
+      UserStore.isLoggedIn = true;
+      UserStore.username = result.username;
+    }
+    else{
+      UserStore.loadiing = false;
+      UserStore.isLoggedIn= false;
+    }
+  }
+  catch(e){
+UserStore.loading = false;
+UserStore.isLoggedIn= false;
+  }
+}
 
   return (
     <div className="App">
